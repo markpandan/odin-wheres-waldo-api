c56622491b3bac26c210943f36ac6905d0120c22
markpandan <72681401+markpandan@users.noreply.github.com>
2025-07-17 14:13:52 +0800

    Build routes

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const gamesRoute = require("./routes/GamesRoute");
+app.use("/games", gamesRoute);
+
+const highscoresRoute = require("./routes/HighScoresRoute");
+app.use("/highscores", highscoresRoute);
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+  console.log(`Link is http://localhost:${PORT}/`);
+});
