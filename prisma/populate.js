77a536f4fc6029c8d1a0776584ea6052c08d36b4
markpandan <72681401+markpandan@users.noreply.github.com>
2025-07-29 17:48:06 +0800

    Configure populate db

--- prisma/populate.js
+++ prisma/populate.js
@@ -6,35 +6,42 @@ async function populate() {
   await prisma.gameImages.create({
     data: {
       name: "Genshin Impact",
-      imageUrl: "/src/assets/games/1/genshin-impact.jpeg",
-      thumbnailUrl: "/src/assets/games/1/genshin-impact-small.jpeg",
+      imageUrl:
+        "https://res.cloudinary.com/dbgceo61v/image/upload/genshin-impact_xif7io.jpg",
+      thumbnailUrl:
+        "https://res.cloudinary.com/dbgceo61v/image/upload/genshin-impact-small_iwevgf.jpg",
       width: 8000,
       height: 4499,
       Entities: {
         create: [
           {
             name: "Collei",
-            imageUrl: "/src/assets/games/1/entity-Collei.webp",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-Collei_edjdg4.webp",
             box: [550, 1328, 880, 1738],
           },
           {
             name: "Albedo",
-            imageUrl: "/src/assets/games/1/entity-Albedo.webp",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-Albedo_rrdgdf.webp",
             box: [5100, 732, 5290, 981],
           },
           {
             name: "Sethos",
-            imageUrl: "/src/assets/games/1/entity-Sethos.webp",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-Sethos_ejeyar.webp",
             box: [7505, 1108, 7725, 1387],
           },
           {
             name: "Lynette",
-            imageUrl: "/src/assets/games/1/entity-Lynette.webp",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-Lynette_y04rl1.webp",
             box: [643, 2820, 1230, 3197],
           },
           {
             name: "Kokomi",
-            imageUrl: "/src/assets/games/1/entity-Kokomi.webp",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-Kokomi_wylp1h.webp",
             box: [1717, 1150, 1979, 1471],
           },
         ],
@@ -45,35 +52,42 @@ async function populate() {
   await prisma.gameImages.create({
     data: {
       name: "Classic Animes",
-      imageUrl: "/src/assets/games/2/classic-animes.jpg",
-      thumbnailUrl: "/src/assets/games/2/classic-animes-small.jpg",
+      imageUrl:
+        "https://res.cloudinary.com/dbgceo61v/image/upload/classic-animes_a7bgmv.jpg",
+      thumbnailUrl:
+        "https://res.cloudinary.com/dbgceo61v/image/upload/classic-animes-small_t8nf9w.jpg",
       width: 700,
       height: 2056,
       Entities: {
         create: [
           {
             name: "Nobita",
-            imageUrl: "/src/assets/games/2/entity-Nobita.webp",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-Nobita_nkppvq.webp",
             box: [478, 1209, 526, 1254],
           },
           {
             name: "Gon",
-            imageUrl: "/src/assets/games/2/entity-Gon.webp",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-Gon_xemob9.webp",
             box: [367, 318, 421, 391],
           },
           {
             name: "Elric",
-            imageUrl: "/src/assets/games/2/entity-Elric.webp",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-Elric_dgvisv.webp",
             box: [319, 478, 385, 548],
           },
           {
             name: "Astro Boy",
-            imageUrl: "/src/assets/games/2/entity-AstroBoy.webp",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-AstroBoy_db0uiq.webp",
             box: [260, 778, 330, 836],
           },
           {
             name: "Pikachu",
-            imageUrl: "/src/assets/games/2/entity-Pikachu.png",
+            imageUrl:
+              "https://res.cloudinary.com/dbgceo61v/image/upload/entity-Pikachu_napxg0.png",
             box: [478, 1445, 521, 1475],
           },
         ],
